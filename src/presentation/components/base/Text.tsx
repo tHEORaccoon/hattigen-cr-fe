a8a810ec0befc6282ace6d77e27f788a7eaeba51
SammyRaccoon <samuel.obugyei@coderaccoon.com>
2025-02-18 17:07:23 +0000

    Colors, Texts, Buttons
    Colors added to tailwind config, text and button components created. Minor edits on Login Screen

---
+++ src/presentation/components/base/Text.tsx
@@ -0,0 +1,45 @@
+// src/components/Text.tsx
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import clsx from "clsx";
+
+const textVariants = cva("", {
+  variants: {
+    color: {
+      black: "text-[#0A0E0F]",
+      white: "text-white",
+      green: "text-[#18AD36]",
+      blue: "text-[#005AE0]",
+      red: "text-[#FF1A1A]",
+    },
+    variant: {
+      "heading": "text-[32px] font-semibold",
+      "sub-heading": "text-[24px] font-semibold",
+      "body": "text-[15px] font-regular",
+      "footnote": "font-light text-[11px] text-gray",
+      "label-text": "font-regular text-[13px]"
+    },
+  },
+  defaultVariants: {
+    variant: "body",
+    color: "black",
+  },
+});
+
+interface TextProps
+  extends Omit<React.HTMLAttributes<HTMLSpanElement>, 'color'>,
+    VariantProps<typeof textVariants> {
+  children: React.ReactNode;
+}
+
+const Text: React.FC<TextProps> = ({ children, className, variant, color, ...props }) => {
+  const computedClassName = clsx(textVariants({ variant, color }), className);
+
+  return (
+    <p {...props} className={computedClassName} >
+      {children}
+    </p>
+  );
+};
+
+export { Text, textVariants };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ export default {
       fontFamily: {
         poppins: ['"Poppins"'],
       },
+      colors: {
+        'primary': '#18AD36',
+        'primary-light': '#4DDC66',
+        'primary-dark': '#0E7A25',
+        'secondary': '#0A0E0F',
+        'secondary-light': '#33383C',
+        'secondary-dark': '#050607',
+        'gray': '#5C5C5C',
+        'gray-light': '#8C8C8C',
+        'gray-dark': '#3A3A3A',
+        'white': '#FEFEFE',
+        'off-white': '#F5F5F5',
+        'blue': "#005AE0"
+      },
     },
   },
   plugins: [],
