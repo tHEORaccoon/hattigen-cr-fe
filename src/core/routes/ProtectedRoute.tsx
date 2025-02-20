246cb870bbe34aac6787bb239615fa1bdd4ba1c2
Conduah <nathaniel.conduah@dicebridgegmbh.onmicrosoft.com>
2025-02-20 12:47:40 +0000

    Fix: Connect buttons

--- src/core/routes/ProtectedRoute.tsx
+++ src/core/routes/ProtectedRoute.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-
-const isAuthenticated = false;  // Replace this with the actual authentication state
 
 const ProtectedRoute = ({ children }: { children: React.ReactElement }) => {
-//   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  return !isAuthenticated ? children : <Navigate to="/login" />;
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
