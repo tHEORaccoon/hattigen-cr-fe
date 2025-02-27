e659a642001ec9f9ebc99d05eb3637e32286be47
mary <mary.jimoh@coderaccoon.com>
2025-02-27 17:23:34 +0000

    feat:fetched data from profile

--- src/core/redux/store/store.ts
+++ src/core/redux/store/store.ts
@@ -2,11 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../slice/authSlice'
 import cvFormReducer from '../slice/cvFormSlice'
 import personalInfoReducer from '../slice/personalInfoSlice'
+import categoriesReducer from '../../redux/slice/categorySlice'
 
 const rootReducer = {
     auth:authReducer,
     cvForm:cvFormReducer,
     personalInfo:personalInfoReducer,
+    category:categoriesReducer
+
 }
 
 export const store = configureStore({
